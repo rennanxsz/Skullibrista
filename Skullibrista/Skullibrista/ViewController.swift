9740f7cad5f401cd74e07ff9aac55e6ecb92458c
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-06 22:07:52 -0300

    Animation setting
    Configurando a animação a ser realizada.

--- Skullibrista/Skullibrista/ViewController.swift
+++ Skullibrista/Skullibrista/ViewController.swift
@@ -21,6 +21,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         viGameOver.isHidden = true
         
+        //Vazando as imagens para que ao virar o celular não apareceça a borda branca.
+        street.frame.size.width = view.frame.size.width * 2
+        street.frame.size.height = view.frame.size.height * 2
+        //Fixando o street sempre no mieo da tela
+        street.center = view.center
+        
+        //Fixando o player sempre no meio da tela
+        player.center = view.center
+        //Criando um array de imagens a serem animadas
+        player.animationImages = []
+        for i in 0...7 {
+            let image = UIImage(named: "player\(i)")!
+            player.animationImages?.append(image)
+        }
+        //Setando o tempo que o array vai percorrer as imagens
+        player.animationDuration = 0.5
+        //Iniciando a animação de imagens
+        player.startAnimating()
     }
 
     @IBAction func playAgain(_ sender: UIButton) {
